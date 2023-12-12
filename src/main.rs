298e544772ae28347d259eb8192ecee7438d5da8
wadec <wadec@andrew.cmu.edu>
2023-12-12 15:25:09 -0500

    v0.3.0

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::sync::{Arc, Mutex};
 use device_query::{DeviceEvents, DeviceState, Keycode};
 use std::{thread, time};
 use std::collections::VecDeque;
-
+use rand::thread_rng;
+use rand::seq::SliceRandom;
 
 // const MAP_ROWS: i32 = 20;
 // const MAP_COLS: i32 = 20;
@@ -54,15 +55,14 @@ fn main() {
     apple_locs.push((9,5));
     apple_locs.push((9,7));
     apple_locs.push((9,9));
+    let mut apple_open_locs_1d = [0; 10 * 30]; // apple open locations, saved as a single integer. i,j is known if width and height of map is known
 
+    let mut rng = thread_rng();
 
     let mut i: usize = 0;
     let mut j: usize = 0;
     let q = Arc::clone(&queued_movement);
     'game: loop {
-        for (i,j) in snake_locs.iter() {
-            map.replace_range((j + i*31)..(j + i*31)+1,".");
-        }
         
         let mvt: Movement = *q.lock().unwrap();
         match mvt {
@@ -75,14 +75,40 @@ fn main() {
         
         snake_locs.push_back((i,j));
 
-        for (i,j) in apple_locs.iter() {
-            map.replace_range((j + i*31)..(j + i*31)+1,"o");
-        }
-
         let snake_head_pos = snake_locs.back().unwrap();
         match apple_locs.iter().position(|x| x == snake_head_pos) {
-            None => { snake_locs.pop_front(); },  // if we didn't eat an apple, the snake moves along
-            Some(pos) => { apple_locs.remove(pos); }, // if we did, remove that apple and "grow" by not moving along
+            Some(pos) => { 
+                // if an apple's position is where we just moved to, remove that apple and "grow" by not moving along
+                // spawn a new apple
+                apple_locs.remove(pos); 
+
+                // temp below
+
+                let mut curr_loc_idx = 0; // the index into apple_open_locs_1d
+                for loc_1d in 0..10*30 {
+                    let loc_2d: (usize, usize) = (loc_1d / 30, loc_1d % 30);
+                    println!("{:?}", loc_2d);
+                    if !snake_locs.contains(&loc_2d) && !apple_locs.contains(&loc_2d) {
+                        apple_open_locs_1d[curr_loc_idx] = loc_1d;
+                        curr_loc_idx += 1;
+                    }
+                } // assert(curr_loc_idx is the number of open spaces.)
+
+                // println!("{:?}", &apple_open_locs_1d[0..curr_loc_idx].choose(&mut rng));
+                let new_apple_loc_1d = (&apple_open_locs_1d[0..curr_loc_idx]).choose(&mut rng).unwrap();
+                let new_apple_i = new_apple_loc_1d / 30;
+                let new_apple_j = new_apple_loc_1d % 30;
+                apple_locs.push((new_apple_i, new_apple_j));
+            }, 
+            None => { 
+                // if there is no such position, we didn't eat an apple; the snake just moves along
+                let (tail_i, tail_j) = snake_locs.pop_front().unwrap(); 
+                map.replace_range((tail_j + tail_i*31)..(tail_j + tail_i*31)+1,".");
+            },  
+        }
+
+        for (i,j) in apple_locs.iter() {
+            map.replace_range((j + i*31)..(j + i*31)+1,"o");
         }
 
         let mut snake_locs_iter = snake_locs.iter().peekable();
